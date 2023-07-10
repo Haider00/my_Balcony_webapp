869af7222d95cb621633d204565445eec9486f9b
IrzamTariq <45371285+IrzamTariq@users.noreply.github.com>
2023-07-10 20:59:11 +0500

    set worksapce pages

--- src/pages/workFromIndoor.js
+++ src/pages/workFromIndoor.js
@@ -1,100 +1,62 @@
-import React from 'react'
+import React from "react";
 import { CustomHeader, WebTabs } from "../component";
 import Grid from "@mui/material/Grid";
-import { Box } from '@mui/material';
-import MenuSection from './MenuSection/menuSection'
-import RightIcons from '@components/rightIcons/rightIcons';
-import WorkFromIndoorImage from '@components/workFromIndoorImage/workFromIndoorImage';
+import { Box } from "@mui/material";
+import MenuSection from "./MenuSection/menuSection";
+import RightIcons from "@components/rightIcons/rightIcons";
+import WorkFromIndoorImage from "@components/workFromIndoorImage/workFromIndoorImage";
 import Image from "next/image";
-import WorkFromOutdoorImage from '@components/workFromOutdoorImage/workFromoutdoorImage';
-import Pagination from '@mui/material/Pagination';
+import WorkFromOutdoorImage from "@components/workFromOutdoorImage/workFromoutdoorImage";
+import Pagination from "@mui/material/Pagination";
 import TableBottom from "src/assets/images/tablebottom.png";
-import {Divider} from '@mui/material';
+import { Divider } from "@mui/material";
 // import TableBottom from 'src/assets/svg/TableBottom';
 
 export default function WorkFromIndoor() {
-    return (
-        <>
-            <CustomHeader />
+  return (
+    <>
+      <div
+        style={{
+          marginLeft: 20,
+          marginTop: 150,
+          position: "fixed",
+          zIndex: 10,
+        }}
+      >
+        <WebTabs selectedTab={3} />
+      </div>
+      <div>
+        <CustomHeader />
 
-            <Grid style={{ justifyContent: 'center', alignItems: 'center', maxWidth: 1400, marginLeft: "auto", marginRight: "auto", marginTop: 130 }} sx={{ marginY: 1, }} container >
-                <Grid container sx={{ justifyContent: 'space-between' }}>
-                    <Grid md={1}
-                        lg={1}
-                        xs={1}
-                        sm={12} item>
-                        <WebTabs selectedTab={3} />
-                    </Grid>
-                    <Grid
-                        item
-                        md={12}
-                        lg={4}
-                        xs={12}
-                        sm={12}
-                    >
-                        <WorkFromIndoorImage />
-                    </Grid>
-                    <Grid item
-                        md={12}
-                        lg={1}
-                        xs={12}
-                        sm={12}>
-                            <Divider orientation="vertical" flexItem />
-                    </Grid>
-                    <Grid
-                        item
-                        md={12}
-                        lg={4}
-                        xs={12}
-                        sm={12}
-                    >
-                        <WorkFromOutdoorImage />
-                    </Grid>
-                    <Grid
-                        marginX={6}
-                        sx={{
-                            display: { xs: "none", md: "flex" },
-                            flexDirection: "column",
-                            flex: 1,
-                        }}
-                        item
-                        md={1}
-                        lg={1}
-                        xs={1}
-                        sm={1}
-                    >
-                        <RightIcons />
-                    </Grid>
-                </Grid>
-            </Grid>
-
-            <Grid
+        <Grid container>
+          <Grid item md={12} lg={12} xs={12} sm={12}>
+            <WorkFromIndoorImage />
+          </Grid>
+          <Grid item md={12} lg={12} xs={12} sm={12}>
+            <Grid container>
+              <Grid
+                item
+                md={3}
+                sm={5}
+                xs={12}
                 sx={{
-                    marginTop: 4,
-                    justifyContent: "space-between",
-                    alignItems: "flex-end",
+                  marginBottom: 4,
+                  marginLeft: 7,
                 }}
-                container
-                spacing={2}
-            >
-                <Grid
-                    item
-                    md={3}
-                    sm={5}
-                    xs={12}
-                    sx={{
-                        marginBottom: 4,
-                        marginLeft: 7,
-                    }}
+              >
+                <MenuSection />
+              </Grid>
+              <Grid item md={8} sx={{ display: { xs: "none", md: "flex" } }}>
+                <Box
+                  sx={{ display: "flex", flex: 1, justifyContent: "flex-end" }}
                 >
-                    <MenuSection />
-                </Grid>
-                <Grid item md={8} sx={{ display: { xs: "none", md: "flex" } }}>
-                    <Box sx={{ display: "flex", flex: 1, justifyContent: "flex-end" }}>
-                        <Image src={TableBottom} alt="" />
-                    </Box>
-                </Grid>
+                  <Image src={TableBottom} alt="" />
+                </Box>
+              </Grid>
             </Grid>
-        </>
-    )
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
 }
